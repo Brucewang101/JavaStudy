bc5f3ea1e6e13484c2c686bd56a14d2a098de1d1
1967193915@qq.com <1967193915.com>
2021-12-17 16:03:51 +0800

    Initial commit

---
+++ module01/src/HelloWorld.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        ArrayList list = new ArrayList();
+    }
+}
---
+++ module01/src/hello.java
@@ -0,0 +1,8 @@
+/**
+ * @author wangchenn
+ * @date 2021/12/17 - 15:35
+ */
+public class hello {
+
+
+}
